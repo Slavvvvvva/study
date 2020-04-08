08f289933237cd9266a040448f945d611aed835c
slaavvvvva <slavvvvva@yandex.ua>
2020-04-08 21:12:08 +0200

    domasznee2

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 'use strict';
-let money =prompt("vhoh is your budzet", 25);
+let money = +prompt("vhoh is your budzet", 25);
 let date =prompt("vvedi datu yyyy-mm-dd", '2015-04-13');
+
 let appData={
     byudjet: money,
     timeData: date,
@@ -9,8 +10,33 @@ let appData={
     dopmaney:[],
     saving: false
 };
-let obyazatelnaya= prompt("vvedi statu roshodow");
-let cena =prompt("skolko deneg");
-appData.expenses.obyazatelnaya=cena;
-alert("byudzet na odin den" + cena/30);
-console.log(appData.byudjet);
+
+for (let i = 0; i < 2; i++ ) {
+    let obyazatelnaya= prompt("vvedi statu roshodow");
+    let cena = prompt("skolko deneg");
+
+    if( (typeof(obyazatelnaya)) === 'string' && obyazatelnaya != null && cena != null && obyazatelnaya != '' && cena != '') {
+       console.log("done");
+        appData.expenses[obyazatelnaya] = cena;
+    } else if ( obyazatelnaya == null || cena == null ){
+        alert("vvedena otmena");
+        break;
+    } else {
+        alert("sliszkom dlinnoe ili, nepravelnyi tip , vvedi eszcho raz");
+        i--;
+    }
+    console.log(i);
+}
+appData.maneyPerDay = appData.byudjet/30;
+
+alert("byudzet na odin den " + appData.maneyPerDay);
+
+if (appData.maneyPerDay < 100) {
+    console.log("minimalnuy uroven dostatka");
+} else if(appData.maneyPerDay>100 && appData.maneyPerDay < 2000){
+    console.log("sredniy uroven dostatka");
+} else if(appData.maneyPerDay > 2000) {
+    console.log("dohrena deneg");
+} else{
+    console.log("chtoto poshlo ne tak");
+}
