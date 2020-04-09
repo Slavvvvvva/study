94f0672521dbb4257d4ed4f51fffdac2e6b5e42c
slaavvvvva <slavvvvva@yandex.ua>
2020-04-09 14:38:38 +0200

    урок 3

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
-'use strict';
-let money = +prompt("vhoh is your budzet", 25);
-let date =prompt("vvedi datu yyyy-mm-dd", '2015-04-13');
+'use strikct';
+let date ;
+let money;
+
+function start(){
+    money = +prompt("vhoh is your budzet", 25); 
+    date =prompt("vvedi datu yyyy-mm-dd", '2015-04-13');
+
+    while(isNaN(money)|| money=="" || money==null ) {
+        money = +prompt("vhoh is your budzet", 25);
+    }
+}
+start();
+
+
 
 let appData={
     byudjet: money,
@@ -8,26 +20,31 @@ let appData={
     expenses:{},
     notexpenses:{},
     dopmaney:[],
-    saving: false
+    saving: true
 };
 
-for (let i = 0; i < 2; i++ ) {
-    let obyazatelnaya= prompt("vvedi statu roshodow");
-    let cena = prompt("skolko deneg");
-
-    if( (typeof(obyazatelnaya)) === 'string' && obyazatelnaya != null && cena != null && obyazatelnaya != '' && cena != '') {
-       console.log("done");
-        appData.expenses[obyazatelnaya] = cena;
-    } else if ( obyazatelnaya == null || cena == null ){
-        alert("vvedena otmena");
-        break;
-    } else {
-        alert("sliszkom dlinnoe ili, nepravelnyi tip , vvedi eszcho raz");
-        i--;
+function vvodTrat(){
+    for (let i = 0; i < 2; i++ ) {
+        let obyazatelnaya= prompt("vvedi statu roshodow");
+        let cena = prompt("skolko deneg");
+    
+        if( (typeof(obyazatelnaya)) === 'string' && obyazatelnaya != null && cena != null && obyazatelnaya != '' && cena != '') {
+           console.log("done");
+            appData.expenses[obyazatelnaya] = cena;
+        } else if ( obyazatelnaya == null || cena == null ){
+            alert("vvedena otmena");
+            break;
+        } else {
+            alert("sliszkom dlinnoe ili, nepravelnyi tip , vvedi eszcho raz");
+            i--;
+        }
+        console.log(i);
     }
-    console.log(i);
 }
-appData.maneyPerDay = appData.byudjet/30;
+
+vvodTrat();
+
+appData.maneyPerDay = (appData.byudjet/30).toFixed(1);
 
 alert("byudzet na odin den " + appData.maneyPerDay);
 
@@ -40,3 +57,15 @@ if (appData.maneyPerDay < 100) {
 } else{
     console.log("chtoto poshlo ne tak");
 }
+
+function checkSavings(){
+    if (appData.saving == true) { 
+        let save = +prompt("skolko nakopil");
+        let procent =+prompt("kakoy procent");
+        appData.monthIncome = save/100/12*procent;
+        alert ("dohod za mesyac s depozita: " + appData.monthIncome);
+
+    }
+}
+
+checkSavings();
