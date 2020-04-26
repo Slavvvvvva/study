49f2ecea5e8a77b77fc5ab2a55313d582917c39f
slaavvvvva <slavvvvva@yandex.ua>
2020-04-26 17:49:29 +0200

    done calcusd

--- calc/js/script.js
+++ calc/js/script.js
@@ -20,3 +20,32 @@ inputRub.addEventListener('input', () => {
     });
 
 });
+
+
+//promis
+
+let inputRub = document.getElementById('rub'),
+    inputUsd = document.getElementById('usd');
+
+inputRub.addEventListener('input', function(){
+
+    let request = new XMLHttpRequest();
+    request.open('GET', 'js/current.json');
+    request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+    request.send();
+
+    function funck1() {
+    return new Promise(function(resolve,reject){
+        if (request.readyState === 4 && request.status == 200) {
+            resolve();
+        } else {
+            reject();
+        }
+    });
+   }
+   funck1
+          .then(  () => { let data = JSON.parse(request.response);
+                         inputUsd.value = inputRub.value / data.usd ;})
+          .catch (  () =>  inputUsd.value = "Что-то пошло не так!")
+
+};
